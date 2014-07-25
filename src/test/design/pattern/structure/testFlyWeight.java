df1b7e24f75a5017bbedb7a3d7ec182ad00cf924
hsy <che@gmail.com>
2014-07-25 17:48:57 +0800

    add a fly weight eg.

--- src/test/design/pattern/structure/testFlyWeight.java
+++ src/test/design/pattern/structure/testFlyWeight.java
@@ -1,14 +1,23 @@
 
 package test.design.pattern.structure;
 
+import static org.junit.Assert.*;
+
+import java.awt.Color;
+import java.lang.ref.WeakReference;
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.EnumSet;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.Set;
+import java.util.WeakHashMap;
 
 import org.junit.Test;
 
 /**
+ * http://zh.wikipedia.org/zh/%E4%BA%AB%E5%85%83%E6%A8%A1%E5%BC%8F
  * http://langgufu.iteye.com/blog/2090516
  * 解释一下概念：也就是说在一个系统中如果有多个相同的对象，那么只共享一份就可以了
  * ，不必每个都去实例化一个对象。比如说一个文本系统，每个字母定一个对象，那么大小写字母一共就是52个
@@ -69,6 +78,13 @@ public class testFlyWeight {
         CharFactory charFactory = new CharFactory();
         Word word = charFactory.generate("ABCABCABCABC");
         word.printSelf();
+    }
+
+    @Test
+    public void test4() {
+        FontData fontData1 = FontData.create(10, "楷体", Color.blue, FontEffect.BOLD);
+        FontData fontData2 = FontData.create(10, "楷体", Color.blue, FontEffect.BOLD);
+        assertEquals(fontData1, fontData2);
     }
 }
 
@@ -246,4 +262,73 @@ class Word {
         }
         System.out.println();
     }
+}
+
+enum FontEffect {
+    BOLD, ITALIC, SUPERSCRIPT, SUBSCRIPT, STRIKETHROUGH
+}
+
+final class FontData {
+    /**
+     * A weak hash map will drop unused references to FontData. Values have to
+     * be wrapped in WeakReferences, because value objects in weak hash map are
+     * held by strong references.
+     */
+    private static final WeakHashMap<FontData, WeakReference<FontData>> FLY_WEIGHT_DATA =
+            new WeakHashMap<FontData, WeakReference<FontData>>();
+    private final int pointSize;
+    private final String fontFace;
+    private final Color color;
+    private final Set<FontEffect> effects;
+
+    private FontData(int pointSize, String fontFace, Color color, EnumSet<FontEffect> effects) {
+        this.pointSize = pointSize;
+        this.fontFace = fontFace;
+        this.color = color;
+        this.effects = Collections.unmodifiableSet(effects);
+    }
+
+    public static FontData create(int pointSize, String fontFace, Color color,
+            FontEffect... effects) {
+        EnumSet<FontEffect> effectsSet = EnumSet.noneOf(FontEffect.class);
+        for (FontEffect fontEffect : effects) {
+            effectsSet.add(fontEffect);
+        }
+        // We are unconcerned with object creation cost, we are reducing overall
+        // memory consumption
+        FontData data = new FontData(pointSize, fontFace, color, effectsSet);
+
+        // Retrieve previously created instance with the given values if it
+        // (still) exists
+        WeakReference<FontData> ref = FLY_WEIGHT_DATA.get(data);
+        FontData result = (ref != null) ? ref.get() : null;
+
+        // Store new font data instance if no matching instance exists
+        if (result == null) {
+            FLY_WEIGHT_DATA.put(data, new WeakReference<FontData>(data));
+            result = data;
+        }
+        // return the single immutable copy with the given values
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (obj instanceof FontData) {
+            if (obj == this) {
+                return true;
+            }
+            FontData other = (FontData) obj;
+            return other.pointSize == pointSize && other.fontFace.equals(fontFace)
+                    && other.color.equals(color) && other.effects.equals(effects);
+        }
+        return false;
+    }
+
+    @Override
+    public int hashCode() {
+        return (pointSize * 37 + effects.hashCode() * 13) * fontFace.hashCode();
+    }
+
+    // Getters for the font data, but no setters. FontData is immutable.
 }
